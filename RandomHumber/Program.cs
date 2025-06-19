b0318f38b010bd6ea66a5847b5fad4e0963e45ad
JhonyXG <jhondroidgameplay@gmail.com>
2025-06-19 12:25:05 -0300

    Adicionar arquivos de projeto.

---
+++ RandomHumber/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+
+
+do
+{
+
+ 
+    Console.WriteLine(@"
+▒█▀▀█ █▀▀█ █▀▀▄ █▀▀▄ █▀▀█ █▀▄▀█ 　 ▒█░▒█ █░░█ █▀▄▀█ █▀▀▄ █▀▀ █▀▀█ 
+▒█▄▄▀ █▄▄█ █░░█ █░░█ █░░█ █░▀░█ 　 ▒█▀▀█ █░░█ █░▀░█ █▀▀▄ █▀▀ █▄▄▀ 
+▒█░▒█ ▀░░▀ ▀░░▀ ▀▀▀░ ▀▀▀▀ ▀░░░▀ 　 ▒█░▒█ ░▀▀▀ ▀░░░▀ ▀▀▀░ ▀▀▀ ▀░▀▀");
+    Console.WriteLine("\n Bem vindo ao RandomHumber");
+    Console.WriteLine(" Advinhe o número escolhido por mim ");
+    Console.WriteLine(" \n Digite um número entre 1 e 100");
+    int numberChosen = int.Parse(Console.ReadLine());
+
+    if (numberChosen < 55)
+    {
+        Console.WriteLine("chute mais alto");
+
+    }
+    if (numberChosen > 55)
+    {
+        Console.WriteLine("chute mais baixo");
+    }
+    if (numberChosen == 55)
+    {
+        Console.WriteLine("Parabens amigão você adivinhou!");
+    }
+    if (numberChosen > 100)
+    {
+        Console.WriteLine("amigo??????");
+    }
+    else
+    {
+        Console.WriteLine("Isso não né?");
+    }
+    switch (numberChosen)
+
+    {
+        case 0:
+            Console.WriteLine("Você digitou zero, não é um número válido!");
+
+            break;
+
+
+
+    }
+
+} while (true);
+
+
+
+
+
